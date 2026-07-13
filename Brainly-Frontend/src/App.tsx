6dec456b531ffb4f80b91f1d54cd207c3c8abbd2
PaddaShanmukhaRao <117668821+PaddaShanmukhaRao@users.noreply.github.com>
2026-07-14 01:29:04 +0530

    a

--- Brainly-Frontend/src/App.tsx
+++ Brainly-Frontend/src/App.tsx
@@ -7,7 +7,6 @@ export default function App() {
       <h1 className="text-3xl font-bold text-blue-600 underline">
         <Button onClick={()=>{}} variant="primary" size="sm" startIcon=""  endIcon="" text="Add Content" ></Button>
       </h1> 
-      
     </div>
   )
 }
