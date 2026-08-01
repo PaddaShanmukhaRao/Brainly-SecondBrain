b221ea7b3c8abec6750dcb4d257af79834920ebf
PaddaShanmukhaRao <117668821+PaddaShanmukhaRao@users.noreply.github.com>
2026-08-02 00:32:41 +0530

    Added Card ui component and embeded yt and twitter

--- Brainly-Frontend/src/App.tsx
+++ Brainly-Frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import Button from "./components/ui/button";
+import { Card } from "./components/ui/Card";
 import { PlusIcon } from "./icons/PlusIcon";
 import { ShareIcon } from "./icons/ShareIcon";
 
@@ -15,6 +16,8 @@ export default function App() {
         text="Add Content"
         startIcon={<PlusIcon />}
       ></Button>
+      <Card type="youtube" link="https://www.youtube.com/watch?v=f8KF7taQkU8" title="harkirat video"/>
+      <Card type="twitter" link="https://x.com/AbhinavXJ/status/2083069736448135479" title="flex tweet"/>
     </>
   );
 }
---
+++ Brainly-Frontend/src/components/ui/Card.tsx
@@ -0,0 +1,41 @@
+import { ShareIcon } from "../../icons/ShareIcon";
+interface CardProps {
+  title: string;
+  link: string;
+  type: "twitter" | "youtube";
+}
+export function Card({ title, link, type }: CardProps) {
+  return (
+    <div>
+      <div className=" p-4 bg-white rounded-md border max-w-72 border-gray-200">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center font text-sm">
+            <div className="pr-2 text-gray-400 ">{<ShareIcon />}</div>
+            {title}
+          </div>
+          <div className="flex items-center">
+            <div className="pr-2 text-gray-400">{<ShareIcon />}</div>
+            <div className="pr-2 text-gray-400">{<ShareIcon />}</div>
+          </div>
+        </div>
+        <div className="pt-4">
+          {type === "youtube" ? (
+            <iframe
+              className="w-full rounded-md"
+              src={link.replace("watch", "embed").replace("?v=", "/")}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          ) : (
+            <blockquote className="twitter-tweet">
+              <a href={link.replace("x", "twitter")}></a>
+            </blockquote>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
