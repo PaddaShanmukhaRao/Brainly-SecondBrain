7b043461953971dda8fd0357b470bc7fbe09b079
PaddaShanmukhaRao <117668821+PaddaShanmukhaRao@users.noreply.github.com>
2026-08-01 18:58:31 +0530

    Added buttomn component

--- Brainly-Frontend/src/App.tsx
+++ Brainly-Frontend/src/App.tsx
@@ -1,9 +1,20 @@
 import Button from "./components/ui/button";
+import { PlusIcon } from "./icons/PlusIcon";
+import { ShareIcon } from "./icons/ShareIcon";
 
-export default function App(){
-  return <>
-  <Button variant={"primary"} text={"Hi There"} ></Button>
-  <Button variant="secondary" text="Hello There"></Button>
-
-  </>
+export default function App() {
+  return (
+    <>
+      <Button
+        variant={"primary"}
+        text={"Share Brain"}
+        startIcon={<ShareIcon />}
+      ></Button>
+      <Button
+        variant="secondary"
+        text="Add Content"
+        startIcon={<PlusIcon />}
+      ></Button>
+    </>
+  );
 }
--- Brainly-Frontend/src/components/ui/button.tsx
+++ Brainly-Frontend/src/components/ui/button.tsx
@@ -1,17 +1,21 @@
 import type { ReactElement } from "react";
-interface ButtonProps{
-        variant : "primary"|"secondary",
-        text: string,
-        startIcon?:ReactElement
+interface ButtonProps {
+  variant: "primary" | "secondary";
+  text: string;
+  startIcon?: ReactElement;
 }
-const variantStyles={
-    "primary":"bg-purple-600 text-white",
-    "secondary":"bg-purple-200 text-purple-400"
-}
-const defultStyles = "px-4 py-2 rounded-md"
-export default function Button({variant,text}:ButtonProps){
-    return <button className={`${variantStyles[variant]} ${defultStyles}`}>
-        {text}
+const variantStyles = {
+  primary: "bg-purple-600 text-white",
+  secondary: "bg-purple-200 text-purple-400",
+};
+const defultStyles = "px-4 py-2 rounded-md font-light flex items-center";
+export default function Button({ variant, text, startIcon }: ButtonProps) {
+  return (
+    <button className={`${variantStyles[variant]} ${defultStyles}`}>
+      <div className="pr-2">
+        {startIcon}
+      </div>
+      {text}
     </button>
-    
+  );
 }
